c5bda59af0359ba51f7cbe16f48eb0aa5fb865ef
Victor Fabian Zapata <victor.zapata@globant.com>
2020-01-16 21:03:03 -0500

    initial set for tooltip

---
+++ tool-tip/logic.js
@@ -0,0 +1,12 @@
+/*
+preferred position: top
+bottom,left or right if there not enough space at the top
+
+*/
+$('#HTML element').showToolTip({
+    Title: 'This is the title',
+    Content: 'This is the content',
+    onApprove: function () {
+        // To be called after Okay button is clicked.
+    }
+});
